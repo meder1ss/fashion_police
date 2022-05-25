bd593d5a303952812ae5a034f2553524a816578e
meder1ss <mvaretsa@gmail.com>
2022-05-25 23:59:30 +0300

    Version with: - Picture color defining (by using an algorithm, that analyzes pixels). - Outfit complementing by such characteristics as: type of clothing, color of clothing and collection of clothing. - Searching outfit by its name. - Add/delete buttons. - Upgraded filter. - Choose-boxes. - css. - Java Security and system of authorization.

--- src/main/java/com/example/fashion_police/models/Product.java
+++ src/main/java/com/example/fashion_police/models/Product.java
@@ -1,6 +1,7 @@
 package com.example.fashion_police.models;
 
 import com.example.fashion_police.FashionPoliceApplication;
+import com.example.fashion_police.services.ProductService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -31,8 +32,6 @@ public class Product {
     private String description;
     @Column(name = "price")
     private int price;
-    @Column(name = "author")
-    private String author;
     @Column(name = "company")
     private String company;
     @Column(name = "collection")
@@ -48,6 +47,9 @@ public class Product {
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
     private Image image;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn
+    private User user;
     private LocalDateTime dateOfCreated;
 
     @PrePersist
@@ -61,93 +63,9 @@ public class Product {
 
         ByteArrayInputStream is = new ByteArrayInputStream(image1.getBytes());
         BufferedImage newBi = ImageIO.read(is);
-        String color1 = getHexColor(newBi);
+        String color1 = ProductService.getHexColor(newBi);
 
-        ArrayList<Integer> rgbArr = hexToRGB(color1);
-        color = get_colour_name(rgbArr);
+        ArrayList<Integer> rgbArr = ProductService.hexToRGB(color1);
+        color = ProductService.get_colour_name(rgbArr);
     }
-    public static String get_colour_name(ArrayList<Integer> a) {
-        Map<String, ArrayList<Integer>> mincolors = new HashMap<>();
-        for (String key : FashionPoliceApplication.outfitcolors.keySet()) {
-            ArrayList<Integer> rgb = FashionPoliceApplication.outfitcolors.get(key);
-            int rd = (rgb.get(0) - a.get(0))*(rgb.get(0) - a.get(0));
-            int gd = (rgb.get(1) - a.get(1))*(rgb.get(1) - a.get(1));
-            int bd = (rgb.get(2) - a.get(2))*(rgb.get(2) - a.get(2));
-            mincolors.put(key, new ArrayList<>(Arrays.asList(rd,gd,bd)));
-        }
-        int minimum = 195075 + 1;
-        String minimum_color = "unknown";
-        for (String key : mincolors.keySet()) {
-            ArrayList<Integer> rgb = mincolors.get(key);
-            if (rgb.get(0) + rgb.get(1) + rgb.get(2) <= minimum) {
-                minimum = rgb.get(0) + rgb.get(1) + rgb.get(2);
-                minimum_color = key;
-            }
-        }
-        return minimum_color;
-    }
-    public static ArrayList<Integer> hexToRGB(String hexcolor) {
-        int r = Color.decode(hexcolor).getRed();
-        int g = Color.decode(hexcolor).getGreen();
-        int b = Color.decode(hexcolor).getBlue();
-        return new ArrayList<>(Arrays.asList(r,g,b));
-    }
-    public static String getHexColor(BufferedImage image) {
-
-        Map<Integer, Integer> colorMap = new HashMap<>();
-        int height = image.getHeight();
-        int width = image.getWidth();
-
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                int rgb = image.getRGB(i, j);
-                if (!isGray(getRGBArr(rgb))) {
-                    Integer counter = colorMap.get(rgb);
-                    if (counter == null) {
-                        counter = 0;
-                    }
-                    colorMap.put(rgb, ++counter);
-                }
-            }
-        }
-
-        return getMostCommonColor(colorMap);
-    }
-    private static String getMostCommonColor(Map<Integer, Integer> map) {
-        List<Map.Entry<Integer, Integer>> list = new LinkedList<>(map.entrySet());
-
-        Collections.sort(list, (Map.Entry<Integer, Integer> obj1, Map.Entry<Integer, Integer> obj2)
-                -> ((Comparable) obj1.getValue()).compareTo(obj2.getValue()));
-
-        Map.Entry<Integer, Integer> entry = list.get(list.size() - 1);
-        int[] rgb = getRGBArr(entry.getKey());
-
-        return "#" + Integer.toHexString(rgb[0])
-                + Integer.toHexString(rgb[1])
-                + Integer.toHexString(rgb[2]);
-    }
-    private static int[] getRGBArr(int pixel) {
-        int alpha = (pixel >> 24) & 0xff;
-        int red = (pixel >> 16) & 0xff;
-        int green = (pixel >> 8) & 0xff;
-        int blue = (pixel) & 0xff;
-
-        return new int[]{red, green, blue};
-    }
-    private static boolean isGray(int[] rgbArr) {
-        int rgDiff = rgbArr[0] - rgbArr[1];
-        int rbDiff = rgbArr[0] - rgbArr[2];
-        int tolerance = 10;
-        if (rgDiff > tolerance || rgDiff < -tolerance) {
-            if (rbDiff > tolerance || rbDiff < -tolerance) {
-                return false;
-            }
-        }
-        if (rgbArr[0] + rgbArr[1] + rgbArr[2] <= 90) {
-            return false;
-        }
-        return true;
-    }
-
-
 }
--- src/main/java/com/example/fashion_police/services/ProductService.java
+++ src/main/java/com/example/fashion_police/services/ProductService.java
@@ -2,14 +2,19 @@ package com.example.fashion_police.services;
 import com.example.fashion_police.FashionPoliceApplication;
 import com.example.fashion_police.models.Image;
 import com.example.fashion_police.models.Product;
+import com.example.fashion_police.models.User;
 import com.example.fashion_police.repositories.ProductRepository;
+import com.example.fashion_police.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.security.Principal;
+import java.util.*;
 import java.util.List;
 
 @Service
@@ -17,6 +22,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     public List<Product> listProducts(String title) {
         if (title != null) return productRepository.findByTitle(title);
@@ -45,17 +51,23 @@ public class ProductService {
         }
     }
 
-    public void saveProduct(Product product, MultipartFile file) throws IOException {
+    public void saveProduct(Principal principal, Product product, MultipartFile file) throws IOException {
+        product.setUser(getUserByPrincipal(principal));
         Image image;
         if (file.getSize() != 0) {
             image = toImageEntity(file);
             product.addImageToProduct(image);
         }
-        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(), product.getAuthor());
+        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(), product.getUser().getEmail());
         productRepository.save(product);
     }
 
-    private Image toImageEntity(MultipartFile file) throws IOException {
+    public User getUserByPrincipal(Principal principal) {
+        if (principal == null) return new User();
+        return userRepository.findByEmail(principal.getName());
+    }
+
+    public static Image toImageEntity(MultipartFile file) throws IOException {
         Image image = new Image();
         image.setName(file.getName());
         image.setOriginalFileName(file.getOriginalFilename());
@@ -82,5 +94,88 @@ public class ProductService {
             }
         }
         return null;
+    }
+
+    public static String get_colour_name(ArrayList<Integer> a) {
+        Map<String, ArrayList<Integer>> mincolors = new HashMap<>();
+        for (String key : FashionPoliceApplication.outfitcolors.keySet()) {
+            ArrayList<Integer> rgb = FashionPoliceApplication.outfitcolors.get(key);
+            int rd = (rgb.get(0) - a.get(0))*(rgb.get(0) - a.get(0));
+            int gd = (rgb.get(1) - a.get(1))*(rgb.get(1) - a.get(1));
+            int bd = (rgb.get(2) - a.get(2))*(rgb.get(2) - a.get(2));
+            mincolors.put(key, new ArrayList<>(Arrays.asList(rd,gd,bd)));
+        }
+        int minimum = 195075 + 1;
+        String minimum_color = "unknown";
+        for (String key : mincolors.keySet()) {
+            ArrayList<Integer> rgb = mincolors.get(key);
+            if (rgb.get(0) + rgb.get(1) + rgb.get(2) <= minimum) {
+                minimum = rgb.get(0) + rgb.get(1) + rgb.get(2);
+                minimum_color = key;
+            }
+        }
+        return minimum_color;
+    }
+    public static ArrayList<Integer> hexToRGB(String hexcolor) {
+        int r = Color.decode(hexcolor).getRed();
+        int g = Color.decode(hexcolor).getGreen();
+        int b = Color.decode(hexcolor).getBlue();
+        return new ArrayList<>(Arrays.asList(r,g,b));
+    }
+    public static String getHexColor(BufferedImage image) {
+
+        Map<Integer, Integer> colorMap = new HashMap<>();
+        int height = image.getHeight();
+        int width = image.getWidth();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int rgb = image.getRGB(i, j);
+                if (!isGray(getRGBArr(rgb))) {
+                    Integer counter = colorMap.get(rgb);
+                    if (counter == null) {
+                        counter = 0;
+                    }
+                    colorMap.put(rgb, ++counter);
+                }
+            }
+        }
+
+        return getMostCommonColor(colorMap);
+    }
+    private static String getMostCommonColor(Map<Integer, Integer> map) {
+        List<Map.Entry<Integer, Integer>> list = new LinkedList<>(map.entrySet());
+
+        Collections.sort(list, (Map.Entry<Integer, Integer> obj1, Map.Entry<Integer, Integer> obj2)
+                -> ((Comparable) obj1.getValue()).compareTo(obj2.getValue()));
+
+        Map.Entry<Integer, Integer> entry = list.get(list.size() - 1);
+        int[] rgb = getRGBArr(entry.getKey());
+
+        return "#" + Integer.toHexString(rgb[0])
+                + Integer.toHexString(rgb[1])
+                + Integer.toHexString(rgb[2]);
+    }
+    private static int[] getRGBArr(int pixel) {
+        int alpha = (pixel >> 24) & 0xff;
+        int red = (pixel >> 16) & 0xff;
+        int green = (pixel >> 8) & 0xff;
+        int blue = (pixel) & 0xff;
+
+        return new int[]{red, green, blue};
+    }
+    private static boolean isGray(int[] rgbArr) {
+        int rgDiff = rgbArr[0] - rgbArr[1];
+        int rbDiff = rgbArr[0] - rgbArr[2];
+        int tolerance = 10;
+        if (rgDiff > tolerance || rgDiff < -tolerance) {
+            if (rbDiff > tolerance || rbDiff < -tolerance) {
+                return false;
+            }
+        }
+        if (rgbArr[0] + rgbArr[1] + rgbArr[2] <= 90) {
+            return false;
+        }
+        return true;
     }
 }
